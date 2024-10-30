102b485e84ee89bbb5581ae952a0e005cbcc731b
nban22 <nban22@clc.fitus.edu.vn>
2024-10-30 18:40:19 +0700

    #5-nban22-design-models-of-database

---
+++ talenthive-website/backend/src/models/jobType.ts
@@ -0,0 +1,24 @@
+import { model, Schema, Types } from "mongoose";
+
+interface IJobType {
+    _id: Types.ObjectId;
+    name: string;
+    description: string;
+}
+
+const JobTypeSchema = new Schema<IJobType>({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const JobType = model<IJobType>('JobType', JobTypeSchema);
+
+export default JobType;
