6c218059c860b2331822af0fb0c20a19d1dc641f
LoiNguyennn <nguyenphucloi231@gmail.com>
2024-11-17 11:08:01 +0700

    #113-LoiNguyennn-Handle routes for Get All Companies

--- talenthive-website/backend/src/controllers/companyController.ts
+++ talenthive-website/backend/src/controllers/companyController.ts
@@ -3,20 +3,12 @@ import AppError from "../utils/appError";
 import catchAsync from "../utils/catchAsync";
 import Company from "../models/company";
 
-const getAllCompanies = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const companies = await Company.find().populate("company_manager");
-        res.status(200).json({
-            status: "success",
-            data: {
-                companies,
-            },
-        });
-    } catch(err) {
-        return next(new AppError("Database error", 500));  
-    }
+export const getAllCompanies = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const companies = await Company.find().populate("company_manager");
+    res.status(200).json({
+        status: "success",
+        data: {
+            companies,
+        },
+    });
 });
-
-export { 
-    getAllCompanies 
-};
