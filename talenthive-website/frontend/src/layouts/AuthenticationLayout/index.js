f5f9996f5a5c3b4668aa50a94c11c533d25040d7
nban22 <nban22@clc.fitus.edu.vn>
2024-11-28 09:38:16 +0700

    #167-nban22-Front-end  - Refactoring react routes system

--- talenthive-website/frontend/src/layouts/AuthenticationLayout/index.js
+++ talenthive-website/frontend/src/layouts/AuthenticationLayout/index.js
@@ -1,13 +1,13 @@
+import { Outlet } from "react-router";
 import Logo from "../../components/Logo";
 import styles from '../../styles/layouts/AuthenticationLayout.module.css'
 
-const AuthenticationLayout = ({children}) => {
+const AuthenticationLayout = () => {
     return (
         <div className = {styles.wrapper}>
             <Logo className = {styles.logo}/>
-            <div className = {styles.form}>{children}</div>
+            <div className = {styles.form}><Outlet/></div>
         </div>
-        
     );
 }
  
