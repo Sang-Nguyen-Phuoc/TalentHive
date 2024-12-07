d3165f256f972e8cea387f91ed6790615b237547
LoiNguyennn <nguyenphucloi231@gmail.com>
2024-12-07 20:19:58 +0700

    #184-LoiNguyennn-Mark notification as read

---
+++ talenthive-website/backend/src/routes/notificationRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as notificationController from "../controllers/notificationController";
+
+const router = Router();
+
+router.put("/:notificationId/markAsRead", notificationController.markNotificationAsRead);
+
+
+export default router;
