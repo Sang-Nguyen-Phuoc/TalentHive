bd8ad2603e90bfc5f666126182c8ed38be349bc8
nban22 <nban22@clc.fitus.edu.vn>
2024-12-31 22:49:40 +0700

    #257-nban22-[Deployment] Deployed Backend to Render

--- talenthive-website/backend/src/server.ts
+++ talenthive-website/backend/src/server.ts
@@ -7,7 +7,7 @@ import app from "./app";
 
 
 const port = parseInt(process.env.PORT || "3002", 10);
-const host = process.env.HOST || "localhost";
+const host = process.env.HOST || "0.0.0.0";
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
